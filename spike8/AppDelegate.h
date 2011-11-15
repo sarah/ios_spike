069e41619ab12ca612b6eed524c3e62b5ef4f366
sarah <sarahg.gray@gmail.com>
2011-11-15 16:54:11 -0600

    Initial Commit

---
+++ spike8/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  spike8
+//
+//  Created by sarah gray on 11/15/11.
+//  Copyright (c) 2011 fabled net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ spike8/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  spike8
+//
+//  Created by sarah gray on 11/15/11.
+//  Copyright (c) 2011 fabled net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
